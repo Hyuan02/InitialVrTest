8ed0dc1b8829d4f198c93230980e01ce93790bf6
Hyuan Peixoto <hyuanpeixoto@gmail.com>
2020-07-20 19:27:34 -0300

    update version

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,9 +1,12 @@
 
 let positions = [
-    "4 4 -12",
-    "0 4 -7",
-    "-4 4 -12"
+    "4 2 -12",
+    "0 2 -7",
+    "-4 2 -12"
 ]
+
+const invisibleMaterial = new THREE.MeshBasicMaterial({side: THREE.FrontSide, colorWrite: false});
+const invisibleBackSideMaterial = new THREE.MeshBasicMaterial({side: THREE.BackSide, colorWrite: false});
 AFRAME.registerComponent('changepositiononnext', {
     init: function(){
         let el = this.el;
@@ -31,3 +34,19 @@ AFRAME.registerComponent('changepositiononprevious', {
         
     }
 })
+
+
+AFRAME.registerComponent('shaderhide', {
+    init: function(){
+        let el = this.el;
+        for(let i = 0; i< 4; i++){
+            // console.log(el.object3D.children[i]);
+            if(i%2 == 0){
+                el.object3D.children[i].children[0].material = invisibleBackSideMaterial;
+            }
+            else{
+                el.object3D.children[i].children[0].material = invisibleMaterial;
+            }
+        }
+    }
+});
